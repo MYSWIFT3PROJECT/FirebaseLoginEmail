c97aad610007e7c5e7d2ed7df17fe28735610344
NemSothe <nemsothea13@gmail.com>
2017-02-07 22:10:42 +0700

    new code with firebase

--- FirebaseLoginEmail/ViewController.swift
+++ FirebaseLoginEmail/ViewController.swift
@@ -7,19 +7,66 @@
 //
 
 import UIKit
-
+import FirebaseAuth
 class ViewController: UIViewController {
-//https://www.youtube.com/watch?v=asKXHVUZiIY
-    override func viewDidLoad() {
+    @IBOutlet weak var segmentUrban: UISegmentedControl!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var pwd: UITextField!
+    @IBOutlet weak var signInLabel: UILabel!
+    @IBOutlet weak var signInButton: UIButton!
+    var isCheckState:Bool = true
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func SegmentPressed(_ sender: UISegmentedControl)
+    {
+        isCheckState = !isCheckState
+        if isCheckState
+        {
+            signInLabel.text = "SignIn"
+            signInButton.setTitle("SignIn", for: .normal)
+        }
+        else
+        {
+            signInLabel.text = "Register"
+            signInButton.setTitle("Register", for: .normal)
+        }
     }
+    @IBAction func SignInPressed(_ sender: UIButton)
+    { let uremail = email.text
+        let urpwd = pwd.text
+        if (uremail != nil),(urpwd != nil){
+            if isCheckState{
+                FIRAuth.auth()?.signIn(withEmail: uremail!, password: urpwd!, completion: {(user,error)in
+                    if let u = user {
+                        self.performSegue(withIdentifier: "gotoHome", sender: self)
+                    }else{
+                        //error
+                    }
+                })
+            }
+        
+            
+    }else{
+            if (uremail != nil),(urpwd != nil){
+                if isCheckState{
+                    FIRAuth.auth()?.createUser(withEmail: uremail!, password: urpwd!, completion: {(user,error)in
+                        if let u = user {
+                            self.performSegue(withIdentifier: "gotoHome", sender: self)
+                        }else{
+                            //error
+                        }
+                    })
+                }
 
 
+    }
 }
+}
+}
+
 
